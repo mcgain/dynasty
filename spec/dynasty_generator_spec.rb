51a1325c36755cdbd10b0c043949a26b24d4d9fd
Richard McGain <mcgain@gmail.com>
2012-08-10 15:17:41 -0400

    nesting of dynasty specs

--- spec/dynasty_generator_spec.rb
+++ spec/dynasty_generator_spec.rb
@@ -1,22 +1,32 @@
 require_relative "../app/dynasty"
 
 describe "Dynasty" do
-  it "generates a root person" do
-    dynasty = Dynasty.new
-    dynasty.root.should be_nil
-    dynasty.generate
-    dynasty.root.should be_a Person
+  before do
+    @dynasty = Dynasty.new
+    @dynasty.generate
   end
 
-  it "should have a relationship" do
-    dynasty = Dynasty.new
-    dynasty.generate
-    dynasty.root.relationships.should have(1).items
+  describe "People generation" do
+    it "generates a root person" do
+      @dynasty.root.should be_a Person
+    end
   end
 
-  it "should have some children" do
-#    dynasty.root.relationships.first.children.should have(1).items 
-  end
+  describe "Relationships" do
+    before do
+      @relationships = @dynasty.root.relationships
+    end
 
+    it "should have a relationship" do
+      @relationships.should have(1).items
+    end
+
+    it "should be related to a person" do
+
+    end
+    it "should have some children" do
+      #    dynasty.root.relationships.first.children.should have(1).items 
+    end
+  end
 end
 
