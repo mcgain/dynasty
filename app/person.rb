2387bd0f249c04d4d41903c3be8955a9bfa18816
Richard McGain <mcgain@gmail.com>
2012-08-14 17:27:07 -0400

    A person has direct access to all it's children

--- app/person.rb
+++ app/person.rb
@@ -4,4 +4,12 @@ class Person
   def initialize
     @relationships = []
   end
+
+  def children
+    my_children = []
+    @relationships.each { |relationship|
+     my_children += relationship.children
+    }
+    my_children
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,4 +14,21 @@ describe "Person" do
     person.relationships << Person.new
     person.relationships.count.should == 2
   end
+
+  it "has children" do
+    person = Person.new
+    child_one = stub
+    child_two = stub
+    child_three = stub
+    relationship_one = stub('relationship', {
+      :children => [child_one, child_two]
+    })
+    relationship_two = stub('relationship', {
+      :children => [child_three]
+    })
+    person.relationships << relationship_one << relationship_two
+    person.children.should include(child_one)
+    person.children.should include(child_two)
+    person.children.should include(child_three)
+  end
 end
